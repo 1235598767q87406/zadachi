7ac3c9f00f3ee98934a13339cc0be436e4a6b413
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2024-11-30 11:24:35 +0000

    я решила 18 задачу из 1 блока

---
+++ block1/task18.py
@@ -0,0 +1,26 @@
+# s = 5
+# print(f"{s}, {type(s)}")
+# s = 57
+# print(f"{s}, {type(s)}")
+# s = 6
+# print(f"{s}, {type(s)}")
+# try:
+#     s = -5.2 * s
+# except NameError as e:
+#     print(e)
+# s = 0
+# print(f"{s}, {type(s)}")
+# s = -7.5
+# print(f"{s}, {type(s)}")
+# try:
+#   s = 2 * s
+# except NameError as e:
+#    print(e)
+# s = 45
+# print(f"{s}, {type(s)}")
+# s = -25
+# print(f"{s}, {type(s)}")
+# try:
+#     s = s + k
+# except NameError as e:
+#     print(e)
