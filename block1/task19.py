de357c05c96aa212c4390d38b743073e7db3fa62
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2024-11-30 11:31:24 +0000

    я решила 19 задачу из 1 блока

---
+++ block1/task19.py
@@ -0,0 +1,26 @@
+# x = 10
+# print(f"{x}, {type(x)}")
+# x = -10
+# print(f"{x}, {type(x)}")
+# x = -17.5
+# print(f"{x}, {type(x)}")
+# try:
+#     x = -2 * x
+# except NameError as e:
+#     print(e)
+# x = 60
+# print(f"{x}, {type(x)}")
+# try:
+#     x = x - 1
+# except NameError as e:
+#    print(e)
+# x = 0
+# print(f"{x}, {type(x)}")
+# x = -50
+# print(f"{x}, {type(x)}")
+# x = -25
+# print(f"{x}, {type(x)}")
+# try:
+#     x = x + k
+# except NameError as e:
+#     print(e)
