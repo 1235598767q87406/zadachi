484e1fb2a29b60ba20eb88f3870b43b14e17daad
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2024-11-30 12:01:41 +0000

    я решила 16 задачу из 1 блока

--- block1/task16.py
+++ block1/task16.py
@@ -1,32 +1,14 @@
-# s = 14
-# print(f"{s}, {type(s)}")
-# k = -3
-# print(f"{k}, {type(k)}")
-# try:
-#     d = s + 1
-# except NameError as e:
-#     print(e)
-# try:
-#     s = d
-# except NameError as e:
-#     print(e)
-# try:
-#     k = 2 * s
-# except NameError as e:
-#     print(e)
-s = 0
-# print(f"{s}, {type(s)}")
-# k = 30
-# print(f"{k}, {type(k)}")
-# try:
-#     d = k - 5
-# except NameError as e:
-#     print(e)
-# try:
-#     k = 2 * d
-# except NameError as e:
-#     print(e)
-# try:    
-#     s = k - 100
-# except NameError as e:
-#     print(e)
+from math import sin
+
+x = int(input("введите x: "))
+a = int(input("введите a: "))
+b = int(input("введите b: "))
+c = int(input("введите c: "))
+# print(f"{a} / {b} / {c} = {a / b / c}")
+# print(f"{a} * {b} / {c} = {a * b / c}")
+# print(f"{a} + {b} / {c} = {a + b / c}")
+# print(f"({a} + {b}) / {c} = {(a + b) / c}")
+# print(f"{a} + {b} / {b} + {c} = {a + b / b + c}")
+# print(f"({a} + {b}) / ({b} + {c}) = {(a + b) / (b + c)}")
+print(f"{a} / sin{b} = {a / sin(b)}")
+print(f"1 / 2 * {a} * {b} * sin{x} = {round(1 / 2 * a * b * sin(x)), 2}")
