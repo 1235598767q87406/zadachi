22f203f644a81ada2336b6c138306e7fe2cc3861
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2024-11-30 11:42:59 +0000

    я решила 20 задачу из 1 блока

---
+++ block1/task16.py
@@ -0,0 +1,32 @@
+# s = 14
+# print(f"{s}, {type(s)}")
+# k = -3
+# print(f"{k}, {type(k)}")
+# try:
+#     d = s + 1
+# except NameError as e:
+#     print(e)
+# try:
+#     s = d
+# except NameError as e:
+#     print(e)
+# try:
+#     k = 2 * s
+# except NameError as e:
+#     print(e)
+s = 0
+# print(f"{s}, {type(s)}")
+# k = 30
+# print(f"{k}, {type(k)}")
+# try:
+#     d = k - 5
+# except NameError as e:
+#     print(e)
+# try:
+#     k = 2 * d
+# except NameError as e:
+#     print(e)
+# try:    
+#     s = k - 100
+# except NameError as e:
+#     print(e)
