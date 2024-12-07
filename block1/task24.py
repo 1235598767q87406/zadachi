205073f30c7fa5fbb1b0671da6cc66c7b26909e9
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2024-12-07 10:57:59 +0000

    я решила 24 задачу из 1 блока

---
+++ block1/task24.py
@@ -0,0 +1,6 @@
+# from math import sin, sqrt
+
+# a = int(input("введите a: "))
+# print(f"x = sqrt(2a + sin(3a) / 3,56) = {round(sqrt(2 * a + sin(3 * a) / 3.56), 2)}")
+# x = int(input("введите x: "))
+# print(f"sin((3.2 + sqrt(1 + x)) / 5x) = {round(sin((3.2 + sqrt(1 + x)) / 5 * x), 2)}")
