17637f01f8ae20c6845fd05a9b6b54012a39b951
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2025-03-16 16:48:17 +0000

    доработала

--- home/task25.py
+++ home/task25.py
@@ -1,5 +1,8 @@
 n = int(input("введите число "))
-ch1 = n % 100
-ch2 = ch1 * 10
-ch3 = ch2 + (n // 100)
-print(f"x = {ch3}")
+if n < 10 or n > 1000:
+    print("ошибка")
+else:
+    ch1 = n % 100
+    ch2 = ch1 * 10
+    ch3 = ch2 + (n // 100)
+    print(f"x = {ch3}")
---
+++ home/task26.py
@@ -0,0 +1,5 @@
+chislo = 564
+ch1 = chislo // 10
+ch2 = ch1 * 10
+ch3 = ch2 + (chislo % 10)
+print(f"x = {ch3}")
