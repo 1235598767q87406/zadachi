7ae7f8ecc7b1c0792201513c0b76c10e89260627
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2025-03-30 11:03:26 +0000

    я решила 5 задачу из 3 блока

---
+++ block3/task5.py
@@ -0,0 +1,6 @@
+a = True
+b = False
+c = False
+print(f"1) {a or b and not c}\n2) {not a and not b}\n\
+3) {not (a and c) or b}\n4) {a and not b or c}\n\
+5) {a and (not b or c)}\n6) {a or (not (b and c))}")
