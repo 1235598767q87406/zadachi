c5098c4cf6b039026086ea7a83446d2221620b01
XoNie <149296465+1235598767q87406@users.noreply.github.com>
2025-03-30 12:00:28 +0000

    я решида 11 задачу из 3 блока

---
+++ block3/ task11.py
@@ -0,0 +1,21 @@
+# x = 1
+# y = -1
+# print(f"{x**2 + y**2 <= 4}")
+# x = 1
+# y = 2
+# print(f"{(x >= 0) or (y**2 != 4)}")
+# x = 1
+# y = 2
+# print(f"{(x >= 0) and (y**2 != 4)}")
+# x = 2
+# y = 1
+# print(f"{(x * y != 4) and (x > y)}")
+# x = 2
+# y = 1
+# print(f"{(x * y != 0) and (x < y)}")
+# x = 2
+# y = 1
+# print(f"{(not (x * y < 0)) and (y > x)}")
+# x = 1
+# y = 2
+# print(f"{(not (x * y < 1)) and (y > x)}")
